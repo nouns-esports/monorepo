e102edee5ac41b9bc387a6a6bdfab5ea3a155ec5
Sam Scolari <56174573+Sam-Scolari@users.noreply.github.com>
2023-01-28 11:20:02 -0500

    Update resizeCanvas.ts

--- src/utils/resizeCanvas.ts
+++ src/utils/resizeCanvas.ts
@@ -2,7 +2,8 @@ export default function resizeCanvas(
   canvas: HTMLCanvasElement,
   draw: Function
 ) {
-  canvas.width = window.innerWidth;
+  //   canvas.width = window.innerWidth;
+  canvas.width = window.screen.availWidth;
   //   canvas.height = window.innerHeight;
   canvas.height = window.screen.availHeight;
 
