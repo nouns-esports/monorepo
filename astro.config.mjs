b157584971492a39f3da8c5b694bb9a2609dc251
Sam Scolari <56174573+Sam-Scolari@users.noreply.github.com>
2023-02-19 16:26:35 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   output: "server",
