5015bbd2eed6106d724cdc1a1acb73020ec120ec
Sam Scolari <56174573+Sam-Scolari@users.noreply.github.com>
2023-02-28 11:52:05 -0600

    enhancements

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,16 +1,25 @@
 import { Show, createSignal } from "solid-js";
 
-export default function Alert(props: { message: string }) {
+export default function Alert(props: { message: string; href: string }) {
   const [visible, setVisible] = createSignal(true);
   return (
     <Show when={visible()}>
       <div
         id="alert"
-        onClick={() => setVisible(false)}
-        class="w-full whitespace-nowrap cursor-pointer relative p-2 text-sm z-50 text-white font-semibold flex items-center justify-center bg-red"
+        class="flex relative w-full z-50 p-2 bg-red text-white text-sm  font-semibold items-center justify-center"
       >
-        {props.message}
-        <span class="ml-2">{"\u{1F7AE}"}</span>
+        <a
+          href={props.href}
+          target="_blank"
+          rel="noopener noreferrer"
+          class="w-full whitespace-nowrap flex items-center justify-center"
+        >
+          {props.message}
+          <span class="ml-2">{"🠊"}</span>
+        </a>
+        <span class="cursor-pointer" onClick={(e) => setVisible(false)}>
+          {"\u{1F7AE}"}
+        </span>
       </div>
     </Show>
   );
