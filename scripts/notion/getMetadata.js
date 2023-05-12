f02b166a54d928a052a526ac308e3891b5bcfd13
Sam Scolari <56174573+Sam-Scolari@users.noreply.github.com>
2023-05-11 20:16:36 -0500

    Update getMetadata.js

--- scripts/notion/getMetadata.js
+++ scripts/notion/getMetadata.js
@@ -1,6 +1,9 @@
 export default async function getMetadata(notion) {
   return {
     tagline: "Immortalizing esports through the infinite onchain frontier",
-    banner: undefined,
+    banner: {
+      message: "Want funding to host or compete in a tournament?",
+      url: "https://prop.house/nouns-esports/",
+    },
   };
 }
