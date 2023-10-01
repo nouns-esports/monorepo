94e8c23665c8df1963301a0c71f01bf9b0287221
Sam Scolari <56174573+Sam-Scolari@users.noreply.github.com>
2023-10-01 17:22:59 -0500

    SEO - test

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,8 @@ import Providers from "../providers";
 import { currentLocale } from "next-i18n-router";
 import fetchGames from "@/utils/fetchGames";
 import { Analytics } from "@vercel/analytics/react";
+import baseKeywords from "@/utils/metadata/baseKeywords";
+import i18nConfig from "@/i18nConfig";
 
 const cabin = Cabin({ subsets: ["latin"], variable: "--font-cabin" });
 const luckiestGuy = Luckiest_Guy({
@@ -30,9 +32,40 @@ const londrinaSolid = Londrina_Solid({
   variable: "--font-londrina-solid",
 });
 
+const title = "Nouns Esports";
+const description = "Leading the revolution in community driven esports!";
+
 export const metadata: Metadata = {
-  title: "Nouns Esports",
-  description: "Leading the revolution in community driven esports",
+  title: {
+    default: title,
+    template: `${title} - %s`,
+  },
+  description,
+  keywords: baseKeywords,
+  metadataBase: new URL("https://nouns.gg"),
+  alternates: {
+    canonical: "/",
+    // TODO: See how this works for subpages; is /pt valid for /games/csgo?
+    languages: i18nConfig.locales.reduce((object, locale) => {
+      object[locale] = `/${locale}`;
+      return object;
+    }, {} as Record<string, string>),
+  },
+  openGraph: {
+    type: "website",
+    title,
+    description,
+    images: ["https://nouns.gg/pokemon.webp"],
+    url: "https://nouns.gg",
+  },
+  twitter: {
+    site: "@NounsEsports",
+    card: "summary_large_image",
+    images: ["https://nouns.gg/pokemon.webp"],
+    title,
+    description,
+  },
+  themeColor: "black",
 };
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
