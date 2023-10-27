a9da50fd46ed1ff1333aad01d0d2732d2354ed51
Sam Scolari <56174573+Sam-Scolari@users.noreply.github.com>
2023-10-26 20:29:52 -0500

    Update fetchGames.ts

--- utils/server/fetchGames.ts
+++ utils/server/fetchGames.ts
@@ -1,6 +1,8 @@
 import { db, games } from "@/db/schema";
 import { cache } from "react";
 
+const revalidate = false;
+
 export default cache(async () => {
   return db.select().from(games).orderBy(games.name);
 });
