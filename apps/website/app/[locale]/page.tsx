e1758d0e83cff7a9ab52cc9644854288151bcaf9
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-01-02 19:15:50 -0600

    Update page.tsx

--- apps/website/app/[locale]/page.tsx
+++ apps/website/app/[locale]/page.tsx
@@ -29,9 +29,7 @@ export default async function Home(props: { params: { locale: string } }) {
 
   const projects = await query.projects();
 
-  const isMobile = false;
-
-  //headers().get("x-device-type") === "mobile";
+  const isMobile = headers().get("x-device-type") === "mobile";
 
   return (
     <main className="cursor-crosshair flex flex-col">
