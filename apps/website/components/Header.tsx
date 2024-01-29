d29a8e8072f12209c7b1d92f245a8bde8fc025cc
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-01-28 22:49:36 -0600

    Update Header.tsx

--- apps/website/components/Header.tsx
+++ apps/website/components/Header.tsx
@@ -13,55 +13,57 @@ export default async function Header(props: { locale: string }) {
   const events = await query.events();
 
   return (
-    <div className="flex flex-col items-center w-full">
+    <>
       <Banner events={events} locale={props.locale} />
-      <header className="sticky top-0 w-full z-20 max-w-[1920px]">
-        <div className="absolute top-0 w-full flex items-center justify-between p-8">
-          <div className="flex items-center gap-8">
-            <Link
-              href={`/${props.locale}/`}
-              className="flex gap-4 group items-center cursor-pointer select-none"
-            >
-              <Logo className="group-hover:rotate-[14deg] w-12 transition-transform duration-150" />
-              <p className="text-white font-luckiest-guy text-4xl select-none">
-                Nouns
-              </p>
-            </Link>
-            <div className="max-[500px]:hidden">
-              <SelectLanguage />
+      <header className="sticky top-0 w-full z-20 flex justify-center">
+        <div className="relative w-full max-w-[1920px]">
+          <div className="absolute top-0 w-full flex items-center justify-between p-8">
+            <div className="flex items-center gap-8">
+              <Link
+                href={`/${props.locale}/`}
+                className="flex gap-4 group items-center cursor-pointer select-none"
+              >
+                <Logo className="group-hover:rotate-[14deg] w-12 transition-transform duration-150" />
+                <p className="text-white font-luckiest-guy text-4xl select-none">
+                  Nouns
+                </p>
+              </Link>
+              <div className="max-[500px]:hidden">
+                <SelectLanguage />
+              </div>
             </div>
+            <nav className="flex items-center gap-8 cursor-pointer">
+              <div className="flex gap-6 max-lg:hidden">
+                <HeaderLink href="/getfunded">
+                  <Text en="Get Funded" pt="Seja financiado" />
+                </HeaderLink>
+                <HeaderLink href="https://www.youtube.com/watch?v=SAXzMQ8pPvE">
+                  <Text en="About" pt="Sobre" />
+                </HeaderLink>
+                <HeaderLink href="/shop">
+                  <Text en="Shop" pt="Loja" />
+                </HeaderLink>
+              </div>
+              <Link
+                href="https://pog.nouns.gg"
+                className="flex gap-2 select-none items-center group max-[750px]:hidden"
+              >
+                <Image
+                  src={whiteLogo}
+                  alt="POG logo"
+                  className="w-6 h-6 group-hover:rotate-[14deg] transition-transform duration-150"
+                />
+                <p className="text-white font-luckiest-guy text-xl">POG</p>
+              </Link>
+              <div className="max-[700px]:hidden">
+                <ConnectButton />
+              </div>
+              <Menu locale={props.locale} />
+            </nav>
           </div>
-          <nav className="flex items-center gap-8 cursor-pointer">
-            <div className="flex gap-6 max-lg:hidden">
-              <HeaderLink href="/getfunded">
-                <Text en="Get Funded" pt="Seja financiado" />
-              </HeaderLink>
-              <HeaderLink href="https://www.youtube.com/watch?v=SAXzMQ8pPvE">
-                <Text en="About" pt="Sobre" />
-              </HeaderLink>
-              <HeaderLink href="/shop">
-                <Text en="Shop" pt="Loja" />
-              </HeaderLink>
-            </div>
-            <Link
-              href="https://pog.nouns.gg"
-              className="flex gap-2 select-none items-center group max-[750px]:hidden"
-            >
-              <Image
-                src={whiteLogo}
-                alt="POG logo"
-                className="w-6 h-6 group-hover:rotate-[14deg] transition-transform duration-150"
-              />
-              <p className="text-white font-luckiest-guy text-xl">POG</p>
-            </Link>
-            <div className="max-[700px]:hidden">
-              <ConnectButton />
-            </div>
-            <Menu locale={props.locale} />
-          </nav>
         </div>
       </header>
-    </div>
+    </>
   );
 }
 
