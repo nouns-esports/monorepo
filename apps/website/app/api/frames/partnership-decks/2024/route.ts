12e20cfb7765aed09367c76d89270549bbf3c287
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-02-01 20:46:47 -0600

    mint button on frame

--- apps/website/app/api/frames/partnership-decks/2024/route.ts
+++ apps/website/app/api/frames/partnership-decks/2024/route.ts
@@ -17,11 +17,20 @@ export async function POST(request: NextRequest) {
                                 <meta property="fc:frame:image" content="https://nouns.gg/partnership-decks/2024/2.webp" />
                                 <meta property="fc:frame:button:1" content="Back" />
                                 <meta property="fc:frame:button:2" content="Next" />
+                                <meta property="fc:frame:button:3" content="Mint" />
+                                <meta property="fc:frame:button:3:action" content="post_redirect" />
                                 <meta property="fc:frame:post_url" content="https://nouns.gg/api/frames/partnership-decks/2024?n=2" />
                           </head>
                         </html>
                         `,
       { status: 200, headers: { "content-type": "text/html" } }
+    );
+  }
+
+  if (n === 1 && button === 2) {
+    return NextResponse.redirect(
+      "https://nouns.gg/mint/partnership-deck-2024/",
+      302
     );
   }
 
@@ -34,6 +43,8 @@ export async function POST(request: NextRequest) {
                                 <meta property="fc:frame" content="vNext" />
                                 <meta property="fc:frame:image" content="https://nouns.gg/partnership-decks/2024/1.webp" />
                                 <meta property="fc:frame:button:1" content="Next" />
+                                <meta property="fc:frame:button:2" content="Mint" />
+                                <meta property="fc:frame:button:2:action" content="post_redirect" />
                                 <meta property="fc:frame:post_url" content="https://nouns.gg/api/frames/partnership-decks/2024?n=1" />
                           </head>
                         </html>
@@ -74,6 +85,20 @@ export async function POST(request: NextRequest) {
                         </html>
                         `,
       { status: 200, headers: { "content-type": "text/html" } }
+    );
+  }
+
+  if (n === 16 && button === 2) {
+    return NextResponse.redirect(
+      "https://nouns.gg/mint/partnership-deck-2024/",
+      302
+    );
+  }
+
+  if (button === 3) {
+    return NextResponse.redirect(
+      "https://nouns.gg/mint/partnership-deck-2024/",
+      302
     );
   }
 
