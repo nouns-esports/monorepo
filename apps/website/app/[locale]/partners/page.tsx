a3c4c76ff82c82d7b31cad1866b32a2cc7b53456
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-02-12 18:45:14 -0600

    Update page.tsx

--- apps/website/app/[locale]/partners/page.tsx
+++ apps/website/app/[locale]/partners/page.tsx
@@ -39,7 +39,7 @@ export default function Partners() {
         />
         <meta property="fc:frame:button:1" content="Next" />
         <meta property="fc:frame:button:2" content="Mint" />
-        <meta property="fc:frame:button:2:action" content="minty" />
+        <meta property="fc:frame:button:2:action" content="mint" />
         <meta
           property="fc:frame:button:2:target"
           content="eip155:8453:0xc2edf80bdaf2f067640fb0f3bf695f3dc76b0cf7:6"
