c0c3a0830d329ccdf0029a730d169a426c657116
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-04-01 17:26:30 -0500

    trpc@next, schema update

--- apps/website/app/api/query/client.ts
+++ apps/website/app/api/query/client.ts
@@ -1,6 +1,21 @@
-import { httpBatchLink, createTRPCProxyClient } from "@trpc/client";
+import { httpBatchLink } from "@trpc/client";
+import { createTRPCNext } from "@trpc/next";
 import type { Router } from "@/app/api/[trpc]/router";
+import { getAccessToken } from "@privy-io/react-auth";
 
-export const query = createTRPCProxyClient<Router>({
-  links: [httpBatchLink({ url: "/api" })],
+export const query = createTRPCNext<Router>({
+  config() {
+    return {
+      links: [
+        httpBatchLink({
+          url: "/api",
+          async headers() {
+            return {
+              Authorization: `Bearer ${(await getAccessToken()) || ""}`,
+            };
+          },
+        }),
+      ],
+    };
+  },
 });
--- apps/website/server/db/schema.ts
+++ apps/website/server/db/schema.ts
@@ -5,8 +5,10 @@ import {
   pgEnum,
   pgTable,
   text,
-  time,
+  timestamp,
+  numeric,
   serial,
+  smallint,
 } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
 import { Pool } from "pg";
@@ -90,27 +92,101 @@ export const users = pgTable("users", {
   pass: pass("pass").notNull(),
 });
 
-// export const badges = pgTable("badges", {
-//   id: text("id").primaryKey(),
-//   name: text("name").notNull(),
-//   description: text("description").notNull(),
-//   image: text("image").notNull(),
-//   timestamp: time("timestamp").notNull(),
-// });
+export const badges = pgTable("badges", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  description: text("description").notNull(),
+  image: text("image").notNull(),
+  timestamp: timestamp("timestamp").notNull(),
+});
 
-// export const snapshots = pgTable("snapshots", {
-//   id: serial("id").primaryKey(),
-//   type: text("type").notNull(),
-//   timestamp: time("timestamp").notNull(),
-//   user: text("user").notNull(),
-// });
+// type can be a string or a string:id for things like rounds, but rounds should probably just use a "voter" snapshot type instead and check against the timestamp
+export const snapshots = pgTable("snapshots", {
+  id: serial("id").primaryKey(),
+  type: text("type").notNull(),
+  timestamp: timestamp("timestamp").notNull(),
+  user: text("user").notNull(),
+});
 
-// export const snapshotRelations = relations(snapshots, ({ one }) => ({
-//   user: one(users, {
-//     fields: [snapshots.user],
-//     references: [users.id],
-//   }),
-// }));
+export const snapshotRelations = relations(snapshots, ({ one }) => ({
+  user: one(users, {
+    fields: [snapshots.user],
+    references: [users.id],
+  }),
+}));
+
+export const rounds = pgTable("rounds", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  description: text("description").notNull(),
+  start: timestamp("start").notNull(),
+  end: timestamp("end").notNull(),
+  category: text("category").notNull(),
+  image: text("image").notNull(),
+});
+
+export const roundsRelations = relations(rounds, ({ many }) => ({
+  awards: many(awards),
+  proposals: many(proposals),
+}));
+
+// type is defined as a CAIP-10 string with an optional ID for 1155 token ids
+// See https://docs.farcaster.xyz/reference/frames/spec#mint
+// USDC on Base: eip155:8453:0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913
+// ETH on Base: eip155:8453:0x0000000000000000000000000000000000000000
+// An ERC1155: eip155:8453:0xADDRESS:2
+// Value with precision 78 is the min value to account for a uint256
+export const awards = pgTable("awards", {
+  round: text("round").notNull(),
+  place: smallint("place").notNull(),
+  type: text("type").notNull(),
+  value: numeric("value", { precision: 78 }).notNull(),
+});
+
+export const awardsRelations = relations(awards, ({ one }) => ({
+  round: one(rounds, {
+    fields: [awards.round],
+    references: [rounds.id],
+  }),
+}));
+
+// word "who" in the frontend as "tell us about yourself", there will already be basic context as to who is proposing
+export const proposals = pgTable("proposals", {
+  user: text("user").primaryKey(),
+  round: text("round").notNull(),
+  title: text("title").notNull(),
+  who: text("who").notNull(),
+  why: text("why").notNull(),
+  value: numeric("value", { precision: 78 }).notNull(),
+});
+
+export const proposalsRelations = relations(proposals, ({ one }) => ({
+  user: one(users, {
+    fields: [proposals.user],
+    references: [users.id],
+  }),
+  round: one(rounds, {
+    fields: [proposals.round],
+    references: [rounds.id],
+  }),
+}));
+
+export const votes = pgTable("votes", {
+  user: text("user").primaryKey(),
+  round: text("round").notNull(),
+  count: smallint("count").notNull(),
+});
+
+export const votesRelations = relations(votes, ({ one }) => ({
+  user: one(users, {
+    fields: [votes.user],
+    references: [users.id],
+  }),
+  round: one(rounds, {
+    fields: [votes.round],
+    references: [rounds.id],
+  }),
+}));
 
 export const applicationResponses = pgTable("applicationResponses", {
   user: text("user").primaryKey(),
@@ -149,6 +225,12 @@ export const db = drizzle(
       projects,
       users,
       applicationResponses,
+      rounds,
+      awards,
+      proposals,
+      votes,
+      snapshots,
+      badges,
     },
   }
 );
@@ -160,3 +242,9 @@ export type Project = typeof projects.$inferSelect;
 export type Creator = typeof creators.$inferSelect;
 export type User = typeof users.$inferSelect;
 export type ApplicationResponse = typeof applicationResponses.$inferSelect;
+export type Round = typeof rounds.$inferSelect;
+export type Award = typeof awards.$inferSelect;
+export type Proposal = typeof proposals.$inferSelect;
+export type Vote = typeof votes.$inferSelect;
+export type Snapshot = typeof snapshots.$inferSelect;
+export type Badge = typeof badges.$inferSelect;
