9894614acc7b560066fb11c213596c390ea24524
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-04-22 22:53:08 -0500

    Update Banner.tsx

--- apps/website/components/Banner.tsx
+++ apps/website/components/Banner.tsx
@@ -5,7 +5,7 @@ import Text from "./Text";
 import { ArrowRight, Dot } from "phosphor-react-sc";
 import { useContext } from "react";
 import { PrimaryColorContext } from "@/providers/PrimaryColor";
-import { Event } from "@/app/api/resolve/getEvents";
+import { type Event } from "@/server/queries/events";
 import { Locale } from "@/middleware";
 import { usePathname } from "next/navigation";
 import { env } from "@/env";
