ac62ef36f9d501dd1eecbfc49f181bc5f22d18bf
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-04-22 22:43:35 -0500

    Update HighlightedText.tsx

--- apps/website/components/HighlightedText.tsx
+++ apps/website/components/HighlightedText.tsx
@@ -2,7 +2,6 @@
 
 import { useParams } from "next/navigation";
 import type { Locale } from "@/middleware";
-import { motion } from "framer-motion";
 
 export default function HighlightedText(props: {
   text: Record<Locale, string>;
@@ -24,13 +23,10 @@ export default function HighlightedText(props: {
               xmlns="http://www.w3.org/2000/svg"
               className="absolute -bottom-3 -left-1.5 max-sm:-bottom-2 max-sm:-left-0.5 w-full"
             >
-              <motion.path
+              <path
                 d="M1.2417 2.61258C3.04409 2.49947 4.8292 2.15267 6.62401 1.97369C13.6525 1.27281 20.7035 1.11258 27.7814 1.11258C31.5762 1.11258 35.371 1.11258 39.1657 1.11258"
                 strokeLinecap="round"
                 stroke={props.color}
-                transition={{ duration: 1 }}
-                // initial={{ pathLength: 0, stroke: "transparent" }}
-                // whileInView={{ pathLength: 1, stroke: "#51D06D" }}
               />
             </svg>
           )}
