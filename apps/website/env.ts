201bd26abd6cb6167d1f383165e245e4b9974866
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-06-02 13:26:28 -0500

    Update env.ts

--- apps/website/env.ts
+++ apps/website/env.ts
@@ -20,13 +20,11 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_ENVIRONMENT: z.enum(["development", "production"]),
     NEXT_PUBLIC_PRIVY_APP_ID: z.string().min(1),
-    NEXT_PUBLIC_PIMLICO_API_KEY: z.string().min(1),
     NEXT_PUBLIC_MULTIBASE_API_KEY: z.string().min(1),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_ENVIRONMENT: process.env.NEXT_PUBLIC_ENVIRONMENT,
     NEXT_PUBLIC_PRIVY_APP_ID: process.env.NEXT_PUBLIC_PRIVY_APP_ID,
-    NEXT_PUBLIC_PIMLICO_API_KEY: process.env.NEXT_PUBLIC_PIMLICO_API_KEY,
     NEXT_PUBLIC_MULTIBASE_API_KEY: process.env.NEXT_PUBLIC_MULTIBASE_API_KEY,
   },
 });
