5883a2995741614605276709d4048fc9cd1f5fd0
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-06-09 17:11:45 -0500

    old bot stuff

---
+++ apps/bot/commands/ping.ts
@@ -0,0 +1,10 @@
+// import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+
+// export default {
+//   data: new SlashCommandBuilder()
+//     .setName("ping")
+//     .setDescription("Replies with Pong!"),
+//   execute: async (interaction: CommandInteraction) => {
+//     await interaction.reply("Pong!");
+//   },
+// };
