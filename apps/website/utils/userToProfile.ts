f885f94c9fad9264b553c8769cca2894b9c46540
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-07-11 16:46:06 -0500

    Update userToProfile.ts

--- apps/website/utils/userToProfile.ts
+++ apps/website/utils/userToProfile.ts
@@ -1,23 +1,39 @@
 import type { User } from "@privy-io/server-auth";
 
 export function userToProfile(user: User) {
-  return {
-    id: user.id,
-    name:
-      user.farcaster?.displayName ??
-      user.discord?.username?.split("#")[0] ??
-      user.twitter?.username ??
-      `${user.id.substring(10, 15)}`,
-    pfp:
-      user.farcaster?.pfp ??
-      `https://api.cloudnouns.com/v1/pfp?text=${user.id}&background=1`,
-    socials: {
-      twitter: user.twitter
-        ? `https://x.com/${user.twitter.username}`
-        : undefined,
-      farcaster: user.farcaster
-        ? `https://warpcast.com/${user.farcaster.username}`
-        : undefined,
-    },
+  const socials = {
+    twitter: user.twitter
+      ? `https://x.com/${user.twitter.username}`
+      : undefined,
+    farcaster: user.farcaster
+      ? `https://warpcast.com/${user.farcaster.username}`
+      : undefined,
   };
+
+  if (user.farcaster?.displayName && user.farcaster?.pfp) {
+    return {
+      id: user.id,
+      name: user.farcaster.displayName,
+      pfp: user.farcaster.pfp,
+      socials,
+    };
+  }
+
+  if (user.twitter?.username && user.twitter?.profilePictureUrl) {
+    return {
+      id: user.id,
+      name: user.twitter.username,
+      pfp: user.twitter.profilePictureUrl,
+      socials,
+    };
+  }
+
+  if (user.discord?.username) {
+    return {
+      id: user.id,
+      name: user.discord.username,
+      pfp: `https://api.cloudnouns.com/v1/pfp?text=${user.id}&background=1`,
+      socials,
+    };
+  }
 }
