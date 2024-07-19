9040436f749ca50bc12d1562055c6157c8706472
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-07-19 00:35:20 -0500

    Update checkDiscordAccountAge.ts

--- apps/website/utils/checkDiscordAccountAge.ts
+++ apps/website/utils/checkDiscordAccountAge.ts
@@ -2,6 +2,6 @@ export default function checkDiscordAccountAge(id: string) {
   const creationDate = new Date(Number((BigInt(id) >> 22n) + 1420070400000n));
 
   return (
-    creationDate < new Date(new Date().setMonth(new Date().getMonth() - 3))
+    creationDate < new Date(new Date().setMonth(new Date().getMonth() - 1))
   );
 }
