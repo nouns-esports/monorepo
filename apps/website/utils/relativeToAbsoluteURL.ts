2813648a2eebf69532cf004183a0978c82ee3cbf
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-07-29 15:15:55 -0500

    Update relativeToAbsoluteURL.ts

--- apps/website/utils/relativeToAbsoluteURL.ts
+++ apps/website/utils/relativeToAbsoluteURL.ts
@@ -2,6 +2,6 @@ import { env } from "~/env";
 
 export function relativeToAbsoluteURL(path: string) {
   return env.PUBLIC_DOMAIN.startsWith("localhost")
-    ? `http://${env.PUBLIC_DOMAIN}/${path}`
-    : `https://${env.PUBLIC_DOMAIN}/${path}`;
+    ? `http://${env.PUBLIC_DOMAIN}${path}`
+    : `https://${env.PUBLIC_DOMAIN}${path}`;
 }
