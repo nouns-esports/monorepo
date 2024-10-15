a1fd19029c2344c78e977905f3546f9a33ccdd6e
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-10-15 07:20:37 -0500

    Update discussion.ts

--- apps/website/server/queries/discussion.ts
+++ apps/website/server/queries/discussion.ts
@@ -5,22 +5,22 @@ export const getTrendingPosts = cache(
   async () => {
     const responses = await Promise.all([
       neynarClient.fetchFeed("filter", {
-        limit: 100,
+        limit: 5,
         filterType: "channel_id",
         channelId: "nouns-esports",
       }),
       neynarClient.fetchFeed("filter", {
-        limit: 100,
+        limit: 5,
         filterType: "channel_id",
         channelId: "cs2",
       }),
       neynarClient.fetchFeed("filter", {
-        limit: 100,
+        limit: 5,
         filterType: "channel_id",
         channelId: "smash",
       }),
       neynarClient.fetchFeed("filter", {
-        limit: 100,
+        limit: 5,
         filterType: "channel_id",
         channelId: "dota2",
       }),
@@ -28,19 +28,11 @@ export const getTrendingPosts = cache(
 
     const posts = responses.map((response) => response.casts.flat()).flat();
 
-    let channelCount: Record<string, number> = {};
-
     const filtered = posts
       .filter((post) => {
         if (post.text.length < 1) return false;
         if (!post.channel) return false;
         if (post.author.username === "esports") return false;
-
-        channelCount[post.channel.id] = ++channelCount[post.channel.id] || 0;
-
-        if (channelCount[post.channel.id] > 10) {
-          return false;
-        }
 
         return true;
       })
