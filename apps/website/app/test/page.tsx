cd93b081e9fed4e5bb2e9f3ea164dbbd91e2bd1e
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-10-28 14:09:22 -0500

    Update page.tsx

--- apps/website/app/test/page.tsx
+++ apps/website/app/test/page.tsx
@@ -2,7 +2,7 @@ export default function Test() {
   return (
     <iframe
       src="https://clips.twitch.tv/embed?clip=BlueExquisiteBaconHeyGuys-vynEsLJMItjIbj9m&parent=nouns.gg"
-      className="aspect-video h-64 mt-64"
+      className="w-96 h-64 mt-64"
       //   allowfullscreen
     ></iframe>
   );
