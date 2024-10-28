c2fd7365eea5ec3addc73fb4d4ce737a0190bc12
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-10-28 13:35:50 -0500

    Create page.tsx

---
+++ apps/website/app/test/page.tsx
@@ -0,0 +1,10 @@
+export default function Test() {
+  return (
+    <iframe
+      src="https://clips.twitch.tv/embed?clip=BlueExquisiteBaconHeyGuys-vynEsLJMItjIbj9m&parent=nouns.gg"
+      height="300"
+      width="100%"
+      //   allowfullscreen
+    ></iframe>
+  );
+}
