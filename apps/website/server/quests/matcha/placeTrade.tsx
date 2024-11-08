6d47e60fb8eac03381125674fa721c53f532b319
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-11-08 15:25:58 -0600

    Update placeTrade.tsx

--- apps/website/server/quests/matcha/placeTrade.tsx
+++ apps/website/server/quests/matcha/placeTrade.tsx
@@ -2,55 +2,62 @@ import createAction from "../createAction";
 import { env } from "~/env";
 
 export const placeTrade = createAction<{
-  chain?: string;
-  token?: string;
-  symbol?: string;
+	chain?: string;
+	token?: string;
+	symbol?: string;
 }>(async (actionInputs) => {
-  if (actionInputs.chain || actionInputs.token || actionInputs.symbol) {
-    if (!actionInputs.chain) {
-      throw new Error("Chain input missing in action");
-    }
+	if (actionInputs.chain || actionInputs.token || actionInputs.symbol) {
+		if (!actionInputs.chain) {
+			throw new Error("Chain input missing in action");
+		}
 
-    if (!actionInputs.token) {
-      throw new Error("Token input missing in action");
-    }
+		if (!actionInputs.token) {
+			throw new Error("Token input missing in action");
+		}
 
-    if (!actionInputs.symbol) {
-      throw new Error("Symbol input missing in action");
-    }
-  }
+		if (!actionInputs.symbol) {
+			throw new Error("Symbol input missing in action");
+		}
+	}
 
-  return {
-    description: (
-      <p>
-        Place a trade{" "}
-        {actionInputs.token ? (
-          <>
-            for <span className="text-red">${actionInputs.symbol}</span>{" "}
-          </>
-        ) : (
-          <></>
-        )}
-        on <span className="text-red">Matcha</span>
-      </p>
-    ),
-    url: actionInputs.token
-      ? `/matcha/tokens/${actionInputs.chain}/${actionInputs.token}`
-      : "/matcha",
-    check: async (user) => {
-      if (!user.wallet) return false;
+	return {
+		description: (
+			<p>
+				Place a trade{" "}
+				{actionInputs.token ? (
+					<>
+						for <span className="text-red">${actionInputs.symbol}</span>{" "}
+					</>
+				) : (
+					<></>
+				)}
+				on <span className="text-red">Matcha</span>
+			</p>
+		),
+		url: actionInputs.token
+			? `/matcha/tokens/${actionInputs.chain}/${actionInputs.token}`
+			: "/matcha",
+		check: async (user) => {
+			console.log("CHECKING");
+			console.log(user);
+			console.log(user.nexus);
 
-      if (actionInputs.token) {
-        const response = await fetch(
-          "https://trade-history.spaceship.0x.org/v1/graphql",
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              "0x-api-key": env.MATCHA_API_KEY,
-            },
-            body: JSON.stringify({
-              query: `
+			if (!user.wallet) {
+				console.log("NO WALLET");
+				return false;
+			}
+
+			if (actionInputs.token) {
+				const response = await fetch(
+					"https://trade-history.spaceship.0x.org/v1/graphql",
+					{
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+							"0x-api-key": env.MATCHA_API_KEY,
+						},
+						body: JSON.stringify({
+							query: `
             query MyQuery {
               tradesMatcha(
                 limit: 1
@@ -60,33 +67,39 @@ export const placeTrade = createAction<{
               }
             }
           `,
-            }),
-          }
-        );
+						}),
+					},
+				);
 
-        if (!response.ok) return false;
+				if (!response.ok) {
+					console.log("BAD RESPONSE");
+					return false;
+				}
 
-        const trades = (
-          (await response.json()) as {
-            data: { tradesMatcha: Array<{ transactionHash: string }> };
-          }
-        ).data.tradesMatcha;
+				const trades = (
+					(await response.json()) as {
+						data: { tradesMatcha: Array<{ transactionHash: string }> };
+					}
+				).data.tradesMatcha;
 
-        if (trades.length < 1) return false;
+				if (trades.length < 1) {
+					console.log("NO TRADES");
+					return false;
+				}
 
-        return true;
-      }
+				return true;
+			}
 
-      const response = await fetch(
-        "https://trade-history.spaceship.0x.org/v1/graphql",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "0x-api-key": env.MATCHA_API_KEY,
-          },
-          body: JSON.stringify({
-            query: `
+			const response = await fetch(
+				"https://trade-history.spaceship.0x.org/v1/graphql",
+				{
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+						"0x-api-key": env.MATCHA_API_KEY,
+					},
+					body: JSON.stringify({
+						query: `
             query MyQuery {
               tradesMatcha(
                 limit: 1
@@ -96,21 +109,29 @@ export const placeTrade = createAction<{
               }
             }
           `,
-          }),
-        }
-      );
+					}),
+				},
+			);
 
-      if (!response.ok) return false;
+			if (!response.ok) {
+				console.log("BAD RESPONSE");
+				return false;
+			}
 
-      const trades = (
-        (await response.json()) as {
-          data: { tradesMatcha: Array<{ transactionHash: string }> };
-        }
-      ).data.tradesMatcha;
+			const trades = (
+				(await response.json()) as {
+					data: { tradesMatcha: Array<{ transactionHash: string }> };
+				}
+			).data.tradesMatcha;
 
-      if (trades.length < 1) return false;
+			if (trades.length < 1) {
+				console.log("NO TRADES");
+				return false;
+			}
 
-      return true;
-    },
-  };
+			console.log("EVERYTHINGS FINE");
+
+			return true;
+		},
+	};
 });
