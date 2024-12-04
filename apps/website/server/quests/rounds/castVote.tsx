600428eec7438181c3ad5c9562bc13e671d4d592
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-12-03 18:24:25 -0600

    Update castVote.tsx

--- apps/website/server/quests/rounds/castVote.tsx
+++ apps/website/server/quests/rounds/castVote.tsx
@@ -3,37 +3,36 @@ import createAction from "../createAction";
 import { db, rounds, votes, type Round } from "~/packages/db/schema";
 
 export const castVote = createAction<{ round?: string }>(
-  async (actionInputs) => {
-    let round: Round | undefined;
+	async (actionInputs) => {
+		let round: Round | undefined;
 
-    if (actionInputs.round) {
-      round = await db.query.rounds.findFirst({
-        where: eq(rounds.id, actionInputs.round),
-      });
-    }
+		if (actionInputs.round) {
+			round = await db.query.rounds.findFirst({
+				where: eq(rounds.id, actionInputs.round),
+			});
+		}
 
-    return {
-      description: round ? (
-        <p>
-          Vote on a proposal in
-          <span className="text-red">{round.name}</span>
-        </p>
-      ) : (
-        <p>Vote on any proposal</p>
-      ),
-      url: round ? `/rounds/${round.id}` : "/rounds",
-      check: async (user) => {
-        const vote = await db.query.votes.findFirst({
-          where: and(
-            eq(votes.user, user.id),
-            round ? eq(votes.round, round.id) : undefined
-          ),
-        });
+		return {
+			description: round ? (
+				<p>
+					Vote on a proposal in <span className="text-red">{round.name}</span>
+				</p>
+			) : (
+				<p>Vote on any proposal</p>
+			),
+			url: round ? `/rounds/${round.id}` : "/rounds",
+			check: async (user) => {
+				const vote = await db.query.votes.findFirst({
+					where: and(
+						eq(votes.user, user.id),
+						round ? eq(votes.round, round.id) : undefined,
+					),
+				});
 
-        if (!vote) return false;
+				if (!vote) return false;
 
-        return true;
-      },
-    };
-  }
+				return true;
+			},
+		};
+	},
 );
