f7e609cd87cb4eb0cbc098fa01848ffdb0c97e21
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-12-05 12:58:04 -0500

    Update readNotifications.ts

--- apps/website/server/mutations/readNotifications.ts
+++ apps/website/server/mutations/readNotifications.ts
@@ -3,7 +3,6 @@
 import { onlyUser } from ".";
 import { db, notifications } from "~/packages/db/schema";
 import { eq } from "drizzle-orm";
-import { revalidateTag } from "next/cache";
 
 export const readNotifications = onlyUser.action(async ({ ctx }) => {
 	await db
