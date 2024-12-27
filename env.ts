3b9446c267dad31946b87cb610bf56455e1da4e5
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-12-27 12:07:12 -0600

    Update env.ts

--- env.ts
+++ env.ts
@@ -6,29 +6,31 @@ console.log("before", process.env);
 dotenv.config();
 console.log("after", process.env);
 
-export const env = createEnv({
-	server: {
-		GOOGLE_API_KEY: z.string().min(1),
-		NEYNAR_API_KEY: z.string().min(1),
-		DATABASE_URL: z.string().url(),
-		PRIVY_APP_SECRET: z.string().min(1),
-		PRIVY_VERIFICATION_KEY: z.string().min(1),
-		PRIVY_WEBHOOK_SIGNING_KEY: z.string().min(1),
-		DISCORD_TOKEN: z.string().min(1),
-		DISCORD_GUILD_ID: z.string().min(1),
-		DISCORD_CLIENT_ID: z.string().min(1),
-		PINATA_JWT: z.string().min(1),
-		MATCHA_API_KEY: z.string().min(1),
-		CGX_AIRDROP_URL: z.string().min(1),
-	},
-	client: {
-		NEXT_PUBLIC_ENVIRONMENT: z.enum(["development", "production"]),
-		NEXT_PUBLIC_PRIVY_APP_ID: z.string().min(1),
-		NEXT_PUBLIC_DOMAIN: z.string().min(1),
-	},
-	experimental__runtimeEnv: {
-		NEXT_PUBLIC_ENVIRONMENT: process.env.NEXT_PUBLIC_ENVIRONMENT,
-		NEXT_PUBLIC_PRIVY_APP_ID: process.env.NEXT_PUBLIC_PRIVY_APP_ID,
-		NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
-	},
-});
+// export const env = createEnv({
+// 	server: {
+// 		GOOGLE_API_KEY: z.string().min(1),
+// 		NEYNAR_API_KEY: z.string().min(1),
+// 		DATABASE_URL: z.string().url(),
+// 		PRIVY_APP_SECRET: z.string().min(1),
+// 		PRIVY_VERIFICATION_KEY: z.string().min(1),
+// 		PRIVY_WEBHOOK_SIGNING_KEY: z.string().min(1),
+// 		DISCORD_TOKEN: z.string().min(1),
+// 		DISCORD_GUILD_ID: z.string().min(1),
+// 		DISCORD_CLIENT_ID: z.string().min(1),
+// 		PINATA_JWT: z.string().min(1),
+// 		MATCHA_API_KEY: z.string().min(1),
+// 		CGX_AIRDROP_URL: z.string().min(1),
+// 	},
+// 	client: {
+// 		NEXT_PUBLIC_ENVIRONMENT: z.enum(["development", "production"]),
+// 		NEXT_PUBLIC_PRIVY_APP_ID: z.string().min(1),
+// 		NEXT_PUBLIC_DOMAIN: z.string().min(1),
+// 	},
+// 	experimental__runtimeEnv: {
+// 		NEXT_PUBLIC_ENVIRONMENT: process.env.NEXT_PUBLIC_ENVIRONMENT,
+// 		NEXT_PUBLIC_PRIVY_APP_ID: process.env.NEXT_PUBLIC_PRIVY_APP_ID,
+// 		NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
+// 	},
+// });
+
+export const env = process.env as Record<string, string>;
