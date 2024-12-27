3ae69f8e66e5399ea75ba8317e53f6f89627cfee
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-12-27 12:18:42 -0600

    Update env.ts

--- env.ts
+++ env.ts
@@ -1,6 +1,7 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
+// This will error if a variable is not set in the globalEnv property in turbo.json
 export const env = createEnv({
 	server: {
 		GOOGLE_API_KEY: z.string().min(1),
