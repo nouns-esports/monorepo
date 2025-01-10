283f4f0b639f32ffb773039ddaa85e2ca5adcdcf
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2025-01-10 14:15:15 -0600

    Update discordSnapshot.ts

--- apps/bot/commands/discordSnapshot.ts
+++ apps/bot/commands/discordSnapshot.ts
@@ -3,77 +3,79 @@ import { createCommand } from "../createCommand";
 import { and, gte, lte, desc, inArray, eq } from "drizzle-orm";
 
 export const discordSnapshot = createCommand({
-  description: "Creates a snapshot of Discord users in a voice channel",
-  params: [
-    {
-      type: "string",
-      name: "tag",
-      description: "Give the snapshot a tag",
-      required: false,
-    },
-  ],
-  onlyAdmin: true,
-  execute: async (interaction) => {
-    if (!interaction) {
-      throw new Error("Can only be run from a Discord command");
-    }
+	description: "Creates a snapshot of Discord users in a voice channel",
+	params: [
+		{
+			type: "string",
+			name: "tag",
+			description: "Give the snapshot a tag",
+			required: false,
+		},
+	],
+	onlyAdmin: true,
+	execute: async (interaction) => {
+		console.log("discordSnapshot", interaction);
 
-    const tag = interaction.options.get("tag")?.value?.toString();
+		if (!interaction) {
+			throw new Error("Can only be run from a Discord command");
+		}
 
-    if (!interaction.channel?.isVoiceBased()) {
-      throw new Error("This command can only be run in a voice channel");
-    }
+		const tag = interaction.options.get("tag")?.value?.toString();
 
-    const now = new Date();
+		if (!interaction.channel?.isVoiceBased()) {
+			throw new Error("This command can only be run in a voice channel");
+		}
 
-    const [users, currentSeason] = await Promise.all([
-      db.query.nexus.findMany({
-        where: inArray(
-          nexus.discord,
-          interaction.channel.members.map(
-            (guildMember) => guildMember.user.username
-          )
-        ),
-      }),
-      db.query.seasons.findFirst({
-        where: and(lte(seasons.start, now), gte(seasons.end, now)),
-        orderBy: desc(seasons.start),
-      }),
-    ]);
+		const now = new Date();
 
-    if (!currentSeason) throw new Error("No season found");
+		const [users, currentSeason] = await Promise.all([
+			db.query.nexus.findMany({
+				where: inArray(
+					nexus.discord,
+					interaction.channel.members.map(
+						(guildMember) => guildMember.user.username,
+					),
+				),
+			}),
+			db.query.seasons.findFirst({
+				where: and(lte(seasons.start, now), gte(seasons.end, now)),
+				orderBy: desc(seasons.start),
+			}),
+		]);
 
-    await db.transaction(async (tx) => {
-      for (const user of users) {
-        const [snapshot] = await tx
-          .insert(snapshots)
-          .values({
-            type: "discord-call",
-            user: user.id,
-            tag: tag?.toString(),
-            timestamp: now,
-          })
-          .returning({ id: snapshots.id });
+		if (!currentSeason) throw new Error("No season found");
 
-        const amount = 300;
+		await db.transaction(async (tx) => {
+			for (const user of users) {
+				const [snapshot] = await tx
+					.insert(snapshots)
+					.values({
+						type: "discord-call",
+						user: user.id,
+						tag: tag?.toString(),
+						timestamp: now,
+					})
+					.returning({ id: snapshots.id });
 
-        await tx.insert(xp).values({
-          user: user.id,
-          amount,
-          timestamp: now,
-          snapshot: snapshot.id,
-          season: currentSeason.id,
-        });
+				const amount = 300;
 
-        await tx
-          .update(nexus)
-          .set({
-            xp: user.xp + amount,
-          })
-          .where(eq(nexus.id, user.id));
-      }
-    });
+				await tx.insert(xp).values({
+					user: user.id,
+					amount,
+					timestamp: now,
+					snapshot: snapshot.id,
+					season: currentSeason.id,
+				});
 
-    return "Snapshot created successfully";
-  },
+				await tx
+					.update(nexus)
+					.set({
+						xp: user.xp + amount,
+					})
+					.where(eq(nexus.id, user.id));
+			}
+		});
+
+		return "Snapshot created successfully";
+	},
 });
