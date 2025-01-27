f4fc8c1e9765226670f9c00ceb8c21305037d04e
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2025-01-27 15:43:13 -0600

    fix abi

--- apps/website/server/achievements/becomeDelegate.ts
+++ apps/website/server/achievements/becomeDelegate.ts
@@ -1,14 +1,17 @@
 import type { AuthenticatedUser } from "../queries/users";
 import { viemPublicClients } from "../clients/viem";
+import { parseAbi } from "viem";
 
 export default async function becomeDelegate(user: AuthenticatedUser) {
 	for (const wallet of user.wallets) {
-		const currentVotes = (await viemPublicClients.mainnet.readContract({
+		const currentVotes = await viemPublicClients.mainnet.readContract({
 			address: "0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03",
-			abi: [],
+			abi: parseAbi([
+				"function getCurrentVotes(address) external view returns (uint96)",
+			]),
 			functionName: "getCurrentVotes",
-			args: [wallet.address],
-		})) as bigint;
+			args: [wallet.address as `0x${string}`],
+		});
 
 		if (currentVotes > 1n) {
 			return true;
--- apps/website/server/achievements/lilnouner.ts
+++ apps/website/server/achievements/lilnouner.ts
@@ -1,14 +1,17 @@
 import type { AuthenticatedUser } from "../queries/users";
 import { viemPublicClients } from "../clients/viem";
+import { parseAbi } from "viem";
 
 export default async function lilnouner(user: AuthenticatedUser) {
 	for (const wallet of user.wallets) {
-		const balance = (await viemPublicClients.mainnet.readContract({
+		const balance = await viemPublicClients.mainnet.readContract({
 			address: "0x4b10701bfd7bfedc47d50562b76b436fbb5bdb3b",
-			abi: [],
+			abi: parseAbi([
+				"function balanceOf(address) external view returns (uint256)",
+			]),
 			functionName: "balanceOf",
-			args: [wallet.address],
-		})) as bigint;
+			args: [wallet.address as `0x${string}`],
+		});
 
 		if (balance > 1n) {
 			return true;
