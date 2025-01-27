f46c43b737d6fb46313f51c1057c43c81e73d5d4
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2025-01-27 16:09:22 -0600

    Update becomeDelegate.ts

--- apps/website/server/achievements/becomeDelegate.ts
+++ apps/website/server/achievements/becomeDelegate.ts
@@ -4,16 +4,52 @@ import { parseAbi } from "viem";
 
 export default async function becomeDelegate(user: AuthenticatedUser) {
 	for (const wallet of user.wallets) {
-		const currentVotes = await viemPublicClients.mainnet.readContract({
-			address: "0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03",
-			abi: parseAbi([
-				"function getCurrentVotes(address) external view returns (uint96)",
-			]),
-			functionName: "getCurrentVotes",
-			args: [wallet.address as `0x${string}`],
-		});
+		const [
+			nounsCurrentVotes,
+			lilnounsCurrentVotes,
+			nounsBalance,
+			lilnounsBalance,
+		] = await Promise.all([
+			viemPublicClients.mainnet.readContract({
+				address: "0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03",
+				abi: parseAbi([
+					"function getCurrentVotes(address) external view returns (uint96)",
+				]),
+				functionName: "getCurrentVotes",
+				args: [wallet.address as `0x${string}`],
+			}),
+			viemPublicClients.mainnet.readContract({
+				address: "0x4b10701bfd7bfedc47d50562b76b436fbb5bdb3b",
+				abi: parseAbi([
+					"function getCurrentVotes(address) external view returns (uint96)",
+				]),
+				functionName: "getCurrentVotes",
+				args: [wallet.address as `0x${string}`],
+			}),
+			viemPublicClients.mainnet.readContract({
+				address: "0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03",
+				abi: parseAbi([
+					"function balanceOf(address) external view returns (uint256)",
+				]),
+				functionName: "balanceOf",
+				args: [wallet.address as `0x${string}`],
+			}),
+			viemPublicClients.mainnet.readContract({
+				address: "0x4b10701bfd7bfedc47d50562b76b436fbb5bdb3b",
+				abi: parseAbi([
+					"function balanceOf(address) external view returns (uint256)",
+				]),
+				functionName: "balanceOf",
+				args: [wallet.address as `0x${string}`],
+			}),
+		]);
 
-		if (currentVotes > 1n) {
+		if (
+			nounsBalance > 0n ||
+			lilnounsBalance > 0n ||
+			nounsCurrentVotes > 1n ||
+			lilnounsCurrentVotes > 1n
+		) {
 			return true;
 		}
 	}
