03d2a200b95f91159d753ccdc0561e096679d15f
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2025-01-28 16:53:29 -0600

    Update Privy.tsx

--- apps/website/providers/Privy.tsx
+++ apps/website/providers/Privy.tsx
@@ -12,8 +12,8 @@ import { base, baseSepolia } from "viem/chains";
 import { useEffect, useState } from "react";
 import { create } from "zustand";
 import { SmartWalletsProvider } from "@privy-io/react-auth/smart-wallets";
-// import { useLoginToFrame } from "@privy-io/react-auth/farcaster";
-// import frameSdk from "@farcaster/frame-sdk";
+import { useLoginToFrame } from "@privy-io/react-auth/farcaster";
+import frameSdk from "@farcaster/frame-sdk";
 import { useRouter } from "next/navigation";
 
 export const usePrivyModalState = create<{
@@ -73,10 +73,10 @@ export default function Privy(props: {
 
 function FramesV2(props: { children: React.ReactNode; user?: string }) {
 	const { user, ready, authenticated } = usePrivy();
-	// const { initLoginToFrame, loginToFrame } = useLoginToFrame();
+	const { initLoginToFrame, loginToFrame } = useLoginToFrame();
 
-	// const [context, setContext] = useState<Awaited<typeof frameSdk.context>>();
-	// const [isSDKLoaded, setIsSDKLoaded] = useState(false);
+	const [context, setContext] = useState<Awaited<typeof frameSdk.context>>();
+	const [isSDKLoaded, setIsSDKLoaded] = useState(false);
 
 	const router = useRouter();
 
@@ -94,38 +94,38 @@ function FramesV2(props: { children: React.ReactNode; user?: string }) {
 		}
 	}, [ready, authenticated, user]);
 
-	// // Login to Frame with Privy automatically
-	// useEffect(() => {
-	// 	if (ready && !authenticated) {
-	// 		const login = async () => {
-	// 			const { nonce } = await initLoginToFrame();
-	// 			try {
-	// 				const result = await frameSdk.actions.signIn({ nonce: nonce });
-	// 				await loginToFrame({
-	// 					message: result.message,
-	// 					signature: result.signature,
-	// 				});
-	// 			} catch (error) {
-	// 				console.log("frame signin error", error);
-	// 			}
-	// 		};
+	// Login to Frame with Privy automatically
+	useEffect(() => {
+		if (ready && !authenticated) {
+			const login = async () => {
+				const { nonce } = await initLoginToFrame();
+				try {
+					const result = await frameSdk.actions.signIn({ nonce: nonce });
+					await loginToFrame({
+						message: result.message,
+						signature: result.signature,
+					});
+				} catch (error) {
+					console.log("frame signin error", error);
+				}
+			};
 
-	// 		login();
-	// 	} else if (ready && authenticated) {
-	// 	}
-	// }, [ready, authenticated]);
+			login();
+		} else if (ready && authenticated) {
+		}
+	}, [ready, authenticated]);
 
-	// // Initialize the frame SDK
-	// useEffect(() => {
-	// 	const load = async () => {
-	// 		setContext(await frameSdk.context);
-	// 		frameSdk.actions.ready({});
-	// 	};
-	// 	if (frameSdk && !isSDKLoaded) {
-	// 		setIsSDKLoaded(true);
-	// 		load();
-	// 	}
-	// }, [isSDKLoaded]);
+	// Initialize the frame SDK
+	useEffect(() => {
+		const load = async () => {
+			setContext(await frameSdk.context);
+			frameSdk.actions.ready({});
+		};
+		if (frameSdk && !isSDKLoaded) {
+			setIsSDKLoaded(true);
+			load();
+		}
+	}, [isSDKLoaded]);
 
 	return props.children;
 }
